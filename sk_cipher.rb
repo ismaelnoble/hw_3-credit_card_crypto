f1cea5037672ef1433cd8fb8f087a00bd7551275
KOUTOU Odilon <odilonkoutou@yahoo.fr>
2017-03-22 14:05:21 +0800

    Modern Symmetric Cipher

--- sk_cipher.rb
+++ sk_cipher.rb
@@ -1,13 +1,19 @@
 require 'rbnacl/libsodium'
+require 'base64'
 
 module ModernSymmetricCipher
   def self.generate_new_key
     # TODO: Return a new key as a Base64 string
+    key = RbNaCl::Random.random_bytes(RbNaCl::SecretBox.key_bytes)
+    Base64.strict_encode64(key)
   end
 
   def self.encrypt(document, key)
     # TODO: Return an encrypted string
     #       Use base64 for ciphertext so that it is sendable as text
+    simple_box = RbNaCl::SimpleBox.from_secret_key(key)
+    simple_ciphertext = simple_box.encrypt(document)
+    Base64.strict_encode64(simple_ciphertext)
   end
 
   def self.decrypt(aes_crypt, key)
