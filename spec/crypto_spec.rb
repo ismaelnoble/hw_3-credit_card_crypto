1c280355f403cf15ba2f6df5ad52e809da7e0b5e
KOUTOU Odilon <odilonkoutou@yahoo.fr>
2017-03-22 13:49:19 +0800

    Tests for Modern Cypher

--- spec/crypto_spec.rb
+++ spec/crypto_spec.rb
@@ -1,6 +1,7 @@
 require_relative '../credit_card'
 require_relative '../substitution_cipher'
 require_relative '../double_trans_cipher'
+require_relative '../sk_cipher'
 require 'minitest/autorun'
 
 describe 'Test card info encryption' do
@@ -47,6 +48,20 @@ describe 'Test card info encryption' do
     it 'should decrypt text' do
       enc = DoubleTranspositionCipher.encrypt(@cc, @key)
       dec = DoubleTranspositionCipher.decrypt(enc, @key)
+      dec.must_equal @cc.to_s
+    end
+  end
+
+  describe 'Using ModernSymmetric Cipher' do
+    it 'should encrypt card information' do
+      enc = ModernSymmetricCipher.encrypt(@cc, @key)
+      enc.wont_equal @cc.to_s
+      enc.wont_be_nil
+    end
+
+    it 'should decrypt text' do
+      enc = ModernSymmetricCipher.encrypt(@cc, @key)
+      dec = ModernSymmetricCipher.decrypt(enc, @key)
       dec.must_equal @cc.to_s
     end
   end
